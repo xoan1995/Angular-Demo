d15dc0a5ac36d4abfdf34eebd8d3c1084c8e3c52
xoan1995 <hoanglapnhue@gmail.com>
2021-08-12 17:26:05 +0700

    Bind du lieu

--- src/app/component-book/component-book.component.ts
+++ src/app/component-book/component-book.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-component-book',
+  template: "<h2>Let say hello {{title}}</h2>",
   templateUrl: './component-book.component.html',
   styleUrls: ['./component-book.component.css']
 })
 export class ComponentBookComponent implements OnInit {
 
   constructor() { }
-
+  title = "List books";
   ngOnInit(): void {
   }
-
 }
